7f5f83173b2bcc3fc1a23727175ce7592c9fc72f
Adam MacDonald <adam.macdonald@spoton.com>
2022-11-04 12:20:01 -0400

    doc: document pointer pkg functions

--- pkg/pointer/number.go
+++ pkg/pointer/number.go
@@ -1,5 +1,6 @@
 package pointer
 
+// GetIntP returns a pointer to the num argument.
 func GetIntP(num int) *int {
 	return &num
 }
--- pkg/pointer/string.go
+++ pkg/pointer/string.go
@@ -1,5 +1,6 @@
 package pointer
 
+// GetStringP returns a pointer to the str argument.
 func GetStringP(str string) *string {
 	return &str
 }
